461f1c3333261e40f826b990e0ba1079def72b68
mf217126 <junebug019@gmail.com>
2017-05-15 21:42:22 -0600

    Added a ton of stuff: styled the pages, and added more pages.

--- MyPortfolio/index.js
+++ MyPortfolio/index.js
@@ -1,1 +1,33 @@
+
+//get stuff
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+
+const app = express();
+
+
+const port = 2050;
 console.log("I am here");
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (request, response) =>{
+  response.render('home');
+});
+
+app.use(express.static(path.join(__dirname)));
+app.listen(port,(err) => {
+  if (err){
+    return console.log('uh0h', err)
+  }
+  console.log('server is listening on ${port}')
+});
--- MyPortfolio/server.js
+++ MyPortfolio/server.js
@@ -1,5 +1,7 @@
 //get express
 var express = require('express');
+//get http
+var http = require("http");
 //make an app
 var app = express();
 
@@ -8,10 +10,11 @@ var app = express();
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
+app.use(express.static("./MyPortfolio"));
 
 //start server on port 2050
 
-app.listen(2050);
+http.createServer(app).listen(2050);
 
 // send a message
 console.log('Hey, it works!')
